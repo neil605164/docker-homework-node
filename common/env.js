7ef8aa7aaca401c0e2e930f488f56e9321dfd746
neil605164 <neil605164work@gmail.com>
2018-07-27 16:20:15 +0800

    [修改] 新增heroku設定檔

--- common/env.js
+++ common/env.js
@@ -7,13 +7,19 @@ let ENV = {
         password: 'neil820724', // 資料庫密碼
         database: 'homeworknode', // 資料庫名稱
     },
+    heroku: {
+        DB_host: 'localhost', // DB的IP
+        user: 'postgres', // 資料庫帳號
+        password: 'neil820724', // 資料庫密碼
+        database: 'homeworknode', // 資料庫名稱
+    },
 };
 
 // 載入當前開發環境
 console.log(process.env.NODE_ENV);
 let env = process.env.NODE_ENV;
 if(env == undefined){
-    env = 'local';
+    env = 'heroku';
 }
 module.exports = ENV[env];
 
